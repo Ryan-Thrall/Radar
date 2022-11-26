1eadea22752d5e2b747b37df1f67e73a436c301c
Ryan-Thrall <clear>
2022-11-26 11:44:18 -0700

    setup mysql

---
+++ Radar.client/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="bg-darker text-light">
+    <Navbar />
+  </header>
+  <main class="bg-dark text-light">
+    <router-view />
+  </main>
+  <footer class="bg-dark text-light">
+  </footer>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { AppState } from './AppState'
+import Navbar from './components/Navbar.vue'
+
+export default {
+  setup() {
+    return {
+      appState: computed(() => AppState)
+    }
+  },
+  components: { Navbar }
+}
+</script>
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+:root {
+  --main-height: calc(100vh - 32px - 64px);
+}
+
+
+footer {
+  display: grid;
+  place-content: center;
+  height: 32px;
+}
+</style>
